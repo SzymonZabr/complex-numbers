d66aca009e11566fe262ec929d986d06e6d4067a
SzymonZabr <szymonzabr@gmail.com>
2025-04-06 19:02:42 +0200

    Update complex.cpp

--- complex.cpp
+++ complex.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "complex.h"
+#include "Complex.h"
 
 using namespace std;
 
@@ -16,5 +16,37 @@ complex add_complex(complex a, complex b)
     complex c;
     c.re = a.re + b.re;
     c.im = a.im + b.im;
+    return c;
+}
+
+complex subtract_complex(complex a, complex b)
+{
+    complex c;
+    c.re = a.re - b.re;
+    c.im = a.im - b.im;
+    return c;
+}
+
+complex multiply_complex(complex a, complex b)
+{
+    complex c;
+    c.re = a.re * b.re - a.im * b.im;
+    c.im = a.re * b.im + a.im * b.re;
+    return c;
+}
+
+complex divide_complex(complex a, complex b)
+{
+    complex c;
+    double denominator = b.re * b.re + b.im * b.im;
+    if (denominator == 0)
+    {
+        cout << "Błąd: dzielenie przez zero!" << endl;
+        c.re = 0;
+        c.im = 0;
+        return c;
+    }
+    c.re = (a.re * b.re + a.im * b.im) ;
+    c.im = (a.im * b.re - a.re * b.im) ;
     return c;
 }
